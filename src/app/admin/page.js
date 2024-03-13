91b002f17468720f2d721ebffaaee435fdf42c39
JulianGomez <157065112+julianGomez06@users.noreply.github.com>
2024-03-13 14:06:07 -0500

    rama actualizada

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,4 +1,4 @@
-import NavbarMenu from "@/components/navbar/NavbarMenu.js"
+import NavbarMenu from "@/components/navbar/NavbarMenu";
 import RouteProtected from "@/middleware/RouteProtected";
 
 const page = () => {
@@ -6,8 +6,14 @@ const page = () => {
     
     return(
         <RouteProtected>
-            <div className="w-screen h-screen bg-slate-100 ">
-                hola soy el admin
+            <div className="relative">
+              <div
+                className="fixed top-0 left-0 w-full h-full bg-neutral-800 bg-cover bg-center bg-no-repeat bg-[url('/Fondo.jpg')] z-0"
+              ></div>
+              <div className="relative z-10">
+                <NavbarMenu />
+                
+              </div>
             </div>
         </RouteProtected>
     )
