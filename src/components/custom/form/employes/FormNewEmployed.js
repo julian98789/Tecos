fb87f64e88d6b446a97ee2768a6f4d6834fb79a9
JulianGomez <157065112+julianGomez06@users.noreply.github.com>
2024-03-25 22:10:40 -0500

    cambio from new employee

--- src/components/custom/form/employes/FormNewEmployed.js
+++ src/components/custom/form/employes/FormNewEmployed.js
@@ -39,13 +39,13 @@ const FormNewEmployed = () =>{
     
         <div className=" flex items-center  ">
     
-            <input {...register("name")} {...register("name",{ required: true })}className=" bg-neutral-300 border rounded w-[240px] outline-none h-9  pl-10 placeholder:text-slate-600"  placeholder="Ingrese su nombre"/>
+            <input {...register("name")} {...register("name",{ required: true })}className=" bg-neutral-300 border rounded w-[240px] outline-none h-9  pl-3 placeholder:text-slate-600"  placeholder="Ingrese su nombre"/>
             {errors.name && <span className="text-[#ff0000] text-xs ">Este espacio es requerido</span>}
         </div >
         
         <div className=" flex items-center ">
        
-            <input {...register("lastname")} {...register("lastname",{ required: true })} className="bg-neutral-300  border rounded w-[240px] outline-none h-9  pl-10 placeholder:text-slate-600" placeholder="Ingrese su apellido"/>
+            <input {...register("lastname")} {...register("lastname",{ required: true })} className="bg-neutral-300  border rounded w-[240px] outline-none h-9  pl-3 placeholder:text-slate-600" placeholder="Ingrese su apellido"/>
             {errors.lastname && <span className="text-[#ff0000] text-xs ">Este espacio es requerido</span>}
         </div>
         
@@ -53,42 +53,26 @@ const FormNewEmployed = () =>{
     </div>
     </div>
 
-<<<<<<< HEAD
-    <div className="w-[500px] flex flex-row gap-5">
-    <div className="w-[250px] flex flex-col space-y-3 ">
-    <div className="text-[rgba(141,143,138,255)] text-start ">C.C</div>
-=======
     <div className="w-[500px] flex flex-col space-y-3">
     <div className="flex flex-row gap-28">
     <div className="text-[rgb(247,247,247)] text-start ">Ingresar Contraseña</div>
     <div className="text-[rgb(247,247,247)] text-start ">Correo</div>
     </div>
->>>>>>> e5d20b585d4546df022b958d164abda93d6e82dc
 
     <div className="flex justify-items-start flex-row gap-5">
     
         <div className=" flex items-center ">
-            <input {...register("id")} {...register("id",{ required: true })} className="bg-neutral-300 border rounded w-[240px] outline-none  h-9  pl-10 placeholder:text-slate-600 text-start"  placeholder="Ingrese identificacion"/>
+            <input {...register("id")} {...register("id",{ required: true })} className="bg-neutral-300 border rounded w-[240px] outline-none  h-9  pl-3 placeholder:text-slate-600 text-start"  placeholder="Ingrese identificacion"/>
             
             {errors.id && <span className="text-[#ff0000] text-xs ">Este espacio es requerido</span>}
         </div >
         
-<<<<<<< HEAD
-    </div>
-    </div>
-
-    <div className="w-[250px] flex flex-col space-y-3  ">
-    <div className="text-[rgba(141,143,138,255)] text-start ">Email</div>
-
-    <div className="flex justify-items-start flex-row gap-5">
-=======
->>>>>>> e5d20b585d4546df022b958d164abda93d6e82dc
     
 
    
     
         <div className=" flex items-center ">
-            <input {...register("email")} {...register("email",{ required: true })} className="bg-neutral-300 border  rounded w-[240px] outline-none h-9  pl-10 placeholder:text-slate-600 text-start"  placeholder="Ingrese Email"/>
+            <input {...register("email")} {...register("email",{ required: true })} className="bg-neutral-300 border  rounded w-[240px] outline-none h-9  pl-3 placeholder:text-slate-600 text-start"  placeholder="Ingrese Email"/>
            
             {errors.email && <span className="text-[#ff0000] text-xs ">Este espacio es requerido</span>}
         </div >
@@ -98,29 +82,23 @@ const FormNewEmployed = () =>{
     
 
     <div className="w-[500px] flex flex-col space-y-3 ">
-<<<<<<< HEAD
-    <div className="flex flex-row gap-36">
-    <div className="text-[rgb(141,143,138)] text-start ">Ingresar Contraseña</div>
-    <div className="text-[rgb(141,143,138)] text-start ">Confirmar Contraseña</div>
-=======
     
     <div className="flex flex-row gap-28">
     <div className="text-[rgb(247,247,247)] text-start ">Ingresar Contraseña</div>
     <div className="text-[rgb(247,247,247)] text-start ">Confirmar Contraseña</div>
->>>>>>> e5d20b585d4546df022b958d164abda93d6e82dc
     </div>
 
     <div className="flex justify-center items-center flex-row gap-5">
     
         <div className=" flex items-center ">
-            <input {...register("pass")} {...register("pass",{ required: true })} className="bg-neutral-300 border rounded w-[240px] outline-none h-9  pl-10 placeholder:text-slate-600"  placeholder="Ingrese su contraña"/>
+            <input {...register("pass")} {...register("pass",{ required: true })} className="bg-neutral-300 border rounded w-[240px] outline-none h-9  pl-3 placeholder:text-slate-600"  placeholder="Ingrese su contraña"/>
             
             {errors.pass && <span className="text-[#ff0000] text-xs ">Este espacio es requerido</span>}
         </div >
         
         <div className=" flex items-center ">
        
-            <input {...register("ConfrimPass")} {...register("ConfrimPass",{ required: true })} className="bg-neutral-300  border rounded w-[240px] outline-none h-9  pl-8 placeholder:text-slate-600" placeholder="Confirme su contraseña"/>
+            <input {...register("ConfrimPass")} {...register("ConfrimPass",{ required: true })} className="bg-neutral-300  border rounded w-[240px] outline-none h-9  pl-3 placeholder:text-slate-600" placeholder="Confirme su contraseña"/>
            
             {errors.ConfrimPass && <span className="text-[#ff0000] text-xs ">Este espacio es requerido</span>}
         </div>
@@ -130,11 +108,7 @@ const FormNewEmployed = () =>{
     </div>
 
     <div className="w-[500px] flex flex-col space-y-3  ">
-<<<<<<< HEAD
-    <div className="text-[rgba(141,143,138,255)] text-start ">Seleciona un rol</div>
-=======
     <div className="text-[rgb(247,247,247)] text-start ">Seleciona un rol</div>
->>>>>>> e5d20b585d4546df022b958d164abda93d6e82dc
 
         <div className=" flex items-center ">
       <select id="countries" className=" border  text-sm rounded-lg  w-full p-2.5 bg-neutral-300 ">
