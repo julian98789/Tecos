258549ac8bf622edf3c635744aae562c1b8d7447
JulianGomez <157065112+julianGomez06@users.noreply.github.com>
2024-03-28 14:23:04 -0500

    cambio de la conexion del  pool

--- src/db/MysqlConection.js
+++ src/db/MysqlConection.js
@@ -1,11 +1,21 @@
 import mysql2 from "mysql2/promise";
 
 const pool = mysql2.createPool({
+    host:"monorail.proxy.rlwy.net",
+    user: "root",
+    database: "railway",
+    password: "njTJuzlIXYsmDbmQyNksNpoGvQuHcmsm",
+    port: "19848"
+})
+
+export default pool;
+/*
+const pool = mysql2.createPool({
     host:"roundhouse.proxy.rlwy.net",
     user: "root",
     database: "railway",
     password: "dG2Db3CCEfFbBGfCA6Ac634B2cB1hHCA",
     port: "58740"
 })
+*/
 
-export default pool;
