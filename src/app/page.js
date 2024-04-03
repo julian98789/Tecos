bf147774e975eb5a8d33cf348c4790a074ab2a75
JulianGomez <157065112+julianGomez06@users.noreply.github.com>
2024-04-03 15:07:51 -0500

    mesas enlazadas con la bases de datos

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 'use client'
-import Table from "@/components/custom/tables/Table.js";
+import Table from "@/components/custom/tables/Table";
 import NavBarCliente from "@/components/custom/navbar/NavBarCliente";
 import useStore from "@/hook/useSession.js";
 import { useEffect, useState } from "react";
@@ -10,9 +10,7 @@ export default function Home() {
 
 	const {logged, user, login, logout,getUserData} = useStore()
 	const [val , setVal] = useState(null)
-
 	
-
 	useEffect(()=>{
 
 		console.log(logged)
@@ -22,9 +20,32 @@ export default function Home() {
 	},[logged,user])
 	const role = getUserData();
 
-	const myClick = () =>{
-		
-	}
+  	const [table, setTable] = useState([])
+  	const [url, setUrl]= useState('/api/table')
+  	const recibirDatos = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setTable(data.result)
+  }
+
+	useEffect(()=>{
+		recibirDatos();
+	},[url])
+
+	const updateTableState = async (userId) => {
+
+        const options = {   
+            method: 'PUt',  
+            header: {  
+                'Content-Type': 'application/json'  
+            },
+			body: JSON.stringify({ estado: 'ocupada' })
+ 
+        }
+        
+         await fetch(`/api/table/${userId}`,options)  
+        .then(res=>res.json())    
+    }  
 	
 	return (
 		<div className="w-full h-screen flex justify-center items-center bg-cover  bg-[url('/Fondo.jpg')] bg-top" >
@@ -34,18 +55,11 @@ export default function Home() {
 				<div className="w-full min-h-[calc(100%-60px)]">
 					<div className="w-full min-h-full flex justify-center items-center ">
 						<div className=" gap-4   grid   grid-cols-2 sm:grid-cols-4 lg:gap-6 xl:gap-8">
-							<Table status={true} num={1} />
-							<Table status={true} num={2}/>
-							<Table status={true} num={3}/>
-							<Table status={true} num={4}/>
-							<Table status={true} num={5}/>
-							<Table status={true} num={6}/>
-							<Table status={true} num={7}/>
-							<Table status={true} num={8}/>
-							
-					
-							
-							
+						{
+							table.map((tables)=>{
+							return <Table  tables={tables} updateTableState={updateTableState} />
+							})
+                  		}
 						</div>
 
 					</div>
--- src/components/custom/tables/Table.js
+++ src/components/custom/tables/Table.js
@@ -1,41 +1,50 @@
-'use client'
 
 import { MdTableRestaurant } from "react-icons/md";
+import { useEffect, useState } from "react";
+import useStore from "@/hook/useSession.js";
 
-const Table = ({status,num}) =>{
-    
-    let colorTable;
-    let colorTextTable;
-    let cursor;
-    let transicion;
-    let route;
+const Table = ({ tables, updateTableState }) => {
+    const [clicked, setClicked] = useState(false);
+    const [ocupada, setOcupada] = useState(false);
 
-    switch (status) {
-        case true:
-            colorTable = "bg-zinc-200";
-            colorTextTable = "text-zinc-700";
-            cursor = "cursor-pointer";
-            transicion = "transition-transform , hover:scale-105";
-            route = "/menu" 
-            break;
-        case false:
-            colorTable = "bg-zinc-500";
-            colorTextTable = "text-zinc-900";
-            break;
-        default:
-            break;
-    }
+    useEffect(() => {
+        setOcupada(tables.estado === 'ocupada');
+    }, [tables.estado]);
+
+    const {logged, user, login, logout,getUserData} = useStore()
+	const [val , setVal] = useState(null)
+
+    useEffect(()=>{
+
+		console.log(logged)
+		console.log(user)
+		console.log(getUserData())
+	
+	},[logged,user])
+	const role = getUserData();
+
+
+    const handleClick = async () => {
+        if (role !== 'cajero' && role !== 'admin') { 
+            await updateTableState(tables.id);
+            setClicked(true);
+        }
+    };
+
+   
+
     return (
-        <a href={route}>
-            <div className={`w-[150px] h-[150px] flex justify-center items-center  ${cursor} rounded-lg ${colorTable} hover:transition  ${transicion} duration-300 shadow-2xl`}>
-                <div className="w-full h-[110px] flex flex-col justify-center items-center ">
-                    <MdTableRestaurant className={`text-8xl ${colorTextTable}`}/>
-                    <div className={`w-full text-2xl font-medium  flex justify-center  ${colorTextTable}`}>
-                        Mesa: {num}
-                    </div>
+        <a href="/menu" onClick={handleClick}>
+        <div className={`w-[150px] h-[150px] flex justify-center items-center rounded-lg ${ocupada ? 'bg-zinc-600' : 'bg-zinc-200'} ${ocupada ? 'cursor-default' : 'hover:transition transition-transform hover:scale-105 duration-300 shadow-2xl cursor-pointer'}`}>
+            <div className="w-full h-[110px] flex flex-col justify-center items-center">
+                <MdTableRestaurant className={`text-8xl text-zinc-700`} />
+                <div className={`w-full text-2xl font-medium flex justify-center text-zinc-700`}>
+                    {tables.descripcion}
                 </div>
             </div>
-        </a>
-    )
-}
-export default Table
+        </div>
+    </a>
+    );
+};
+
+export default Table;
