234a59ddcce914cee1de099bb91f6e34a2589cff
KeniBeck <deyler_mena69222@elpoli.edu.co>
2024-04-06 09:31:34 -0500

    actualizacion de formularios

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,8 +1,21 @@
+'use client'
 import RouteProtected from "@/middleware/RouteProtected";
 import Navbar from "@/components/custom/navbar/NavBar";
 import TableEmployes from "@/components/custom/dataTable/TableEmployes";
+import useStore from "@/hook/useSession.js";
+import { useRouter } from "next/navigation";
+
 
 const Admin = () => {
+  const {getUserData} = useStore();
+  const role = getUserData();
+  const router = useRouter()
+
+  if (role === 'cajero'){
+
+    router.push('/cashier')
+
+  }
 
     return (
       <RouteProtected>
