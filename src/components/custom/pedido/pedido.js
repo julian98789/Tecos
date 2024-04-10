c66b36da255a0193915654debd6db9a2b8688021
JulianGomez <157065112+julianGomez06@users.noreply.github.com>
2024-04-10 14:21:53 -0500

    creacion de facturas en formato pdf

--- src/components/custom/pedido/pedido.js
+++ src/components/custom/pedido/pedido.js
@@ -1,22 +1,19 @@
-'use client'
-import { useState, useEffect } from "react"
-import CampoOrder from "../dataTable/order/campoOrder"
+import React, { useState, useEffect } from "react";
+import CampoOrder from "../dataTable/order/campoOrder";
+import LiberarMesa from "./liberarMesa/LiberarMesa";
 
-
-const Pedidos = () =>{
-
+const Pedidos = () => {
     const [order, setOrder] = useState([]);
-    const [url, setUrl] = useState('/api/order');
-
-    const recibirDatos = async () => {
-        const response = await fetch(url);
-        const data = await response.json();
-        setOrder(data.result);
-    }
 
     useEffect(() => {
+        const recibirDatos = async () => {
+            const response = await fetch('/api/order');
+            const data = await response.json();
+            setOrder(data.result);
+        };
+
         recibirDatos();
-    }, [url]);
+    }, []);
 
     const updateCampoOrder = async (orderId) => {
         const options = {   
@@ -28,40 +25,28 @@ const Pedidos = () =>{
         }
         
         await fetch(`/api/order/${orderId}`, options);  
-    }  
+    };  
 
-    const liberarMesa = async (mesaId) => {
-        const options = {   
-            method: 'PUT',  
-            headers: {  
-                'Content-Type': 'application/json'  
-            },
-            body: JSON.stringify({ estado: 'disponible' })
-        }
-        
-        await fetch(`/api/table/${mesaId}`, options);  
-    }
+    // Encontrar mesas únicas
+    const mesasUnicas = [...new Set(order.map(item => item.mesa_id))].sort((a, b) => a - b);
 
     return (
         <div>
-            <p className="text-2xl text-neutral-100 italic  font-semibold mb-2 text-center p-5">Liberar mesas</p>
+            <p className="text-2xl text-neutral-100 italic font-semibold mb-2 text-center p-5">Liberar mesas</p>
             <div className="flex justify-center">
-                <button type="button" className="w-32 h-11 text-sm font-medium text-neutral-100 bg-neutral-900 rounded-lg hover:bg-red-700 mr-2" onClick={() => liberarMesa(1)}>Mesa 1</button>
-                <button type="button" className="w-32 h-11 text-sm font-medium text-neutral-100 bg-neutral-900 rounded-lg hover:bg-red-700 mr-2" onClick={() => liberarMesa(2)}>Mesa 2</button>
-                <button type="button" className="w-32 h-11 text-sm font-medium text-neutral-100 bg-neutral-900 rounded-lg hover:bg-red-700 mr-2" onClick={() => liberarMesa(3)}>Mesa 3</button>
-                <button type="button" className="w-32 h-11 text-sm font-medium text-neutral-100 bg-neutral-900 rounded-lg hover:bg-red-700 mr-2" onClick={() => liberarMesa(4)}>Mesa 4</button>
-                <button type="button" className="w-32 h-11 text-sm font-medium text-neutral-100 bg-neutral-900 rounded-lg hover:bg-red-700 mr-2" onClick={() => liberarMesa(5)}>Mesa 5</button>
-                <button type="button" className="w-32 h-11 text-sm font-medium text-neutral-100 bg-neutral-900 rounded-lg hover:bg-red-700 mr-2" onClick={() => liberarMesa(6)}>Mesa 6</button>
-                <button type="button" className="w-32 h-11 text-sm font-medium text-neutral-100 bg-neutral-900 rounded-lg hover:bg-red-700 mr-2" onClick={() => liberarMesa(7)}>Mesa 7</button>
-                <button type="button" className="w-32 h-11 text-sm font-medium text-neutral-100 bg-neutral-900 rounded-lg hover:bg-red-700" onClick={() => liberarMesa(8)}>Mesa 8</button>
+                {mesasUnicas.map(mesaId => {
+                    // Filtrar las órdenes para encontrar la primera orden asociada a esta mesa
+                    const mesaOrder = order.find(item => item.mesa_id === mesaId);
+                    return mesaOrder ? <LiberarMesa key={mesaOrder.id} liberar={mesaOrder} /> : null;
+                })}
             </div>
             <div className="grid grid-cols-3 gap-5 mx-7 my-4">
                 {order.map((orders) => (
-                    <CampoOrder orders={orders} updateCampoOrder={updateCampoOrder} />
+                    <CampoOrder key={orders.id} orders={orders} updateCampoOrder={updateCampoOrder} />
                 ))}
             </div>
         </div>
     );
-}
+};
 
 export default Pedidos;
